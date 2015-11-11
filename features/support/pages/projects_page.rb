71312ec86409eba05fac0c07294f68f6e4a5fe12
Randy Eppinger <randy.eppinger@leandog.com>
2015-11-10 22:14:57 -0500

    Minor refactor: Reorder parameters.

--- features/support/pages/projects_page.rb
+++ features/support/pages/projects_page.rb
@@ -3,15 +3,14 @@ class ProjectsPage
 
   table(:projects, id: 'projects')
 
-
   def has_project_displayed?(project_name, total_size)
     projects_element.each do |row|
-      return true if row_has_project(project_name, row, total_size)
+      return true if row_has_project(row, project_name, total_size)
     end
     false
   end
 
-  def row_has_project(project_name, row, total_size)
+  def row_has_project(row, project_name, total_size)
     row['Project Name'].text == project_name && row['Total Project Size'].text == total_size
   end
 end
